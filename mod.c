a4983dd812ad4b0a924c4892d39ef69fa148f9cb
tewa92 <tewachewsaleigizer@gmail.com>
2024-01-18 04:52:44 -0800

    Stacks, Queues - LIFO, FIFO

---
+++ mod.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+/**
+ * f_mod - Computes the remainder of the division of the second
+ * top element of the stack by the top element of the stack.
+ * @head: Pointer to the head of the stack
+ * @counter: Line number in the script
+ * Return: No explicit return value
+ */
+
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	aux = h->next->n % h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_nop - Does nothing.
+ * @head: Pointer to the head of the stack
+ * @counter: Line number in the script
+ * Return: No explicit return value
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
